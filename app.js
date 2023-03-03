1ae0b67b3c1f36c58e6d207bb44e6c06e4ec74cc
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-03-03 23:11:22 +0600

    basic info

---
+++ app.js
@@ -0,0 +1,35 @@
+// const saju = "Md Saju";
+// let name = "Md Sohrab Hossain";
+// let secondName = "Sohel";
+
+// //string concat
+
+// let fullName = name + " " + secondName;
+
+// //string concat with template litral
+// let fullSecondName = `${name} asdflja;sdlfj;aslfj  ${secondName}`;
+
+// console.log(fullSecondName);
+
+//if else====>
+
+//loping
+// for (let i = 0; i <= 100; i++) {
+//   console.log("" + i);
+// }
+
+//asdf
+
+switch ("s") {
+  case "s": {
+    console.log("s");
+    break;
+  }
+  case "sk": {
+    console.log("Sk");
+    break;
+  }
+  default: {
+    console.log("Not Matched Anything!1");
+  }
+}
